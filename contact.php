719b0c0a59cef683767400315f79a7a1fd156fd4
DJPorkChop628 <plazar@emich.edu>
2020-08-04 12:19:45 -0400

    monchin on them endless stiks @ da olive garden

--- contact.php
+++ contact.php
@@ -24,10 +24,64 @@
       <section id="main" class="wrapper">
         <div class="inner">
           <div class="content">
-            <p>Interested in getting in contact with Summit Street A Cappella? Fill out the form below, and one of our E-Board members will respond within 72 hours!</p>
-            <h3><?= $alumni["alumni_voice"] ?></h3>
-						<h4><?= $alumni["alumni_tenure"] ?></h4>
-            <p><?= $alumni["alumni_bio"] ?></p>
+            <p>Interested in getting in contact with Summit Street A Cappella? Fill out the form below, and one of our E-Board members will be in touch shortly!</p>
+            <form method="post" action="#">
+              <div class="row gtr-uniform">
+                <div class="col-6 col-12-xsmall">
+                  <input type="text" name="name" id="name" value="" placeholder="Name" />
+                </div>
+                <div class="col-6 col-12-xsmall">
+                  <input type="email" name="email" id="email" value="" placeholder="Email" />
+                </div>
+                <!-- Break -->
+                <div class="col-12">
+                  <select name="category" id="category">
+                    <option value="">- Select -</option>
+                    <option value="alpha">Option alpha</option>
+                    <option value="beta">Option beta</option>
+                    <option value="gamma">Option gamma</option>
+                    <option value="delta">Option delta</option>
+                    <option value="epsilon">Option epsilon</option>
+                    <option value="zeta">Option zeta</option>
+                    <option value="eta">Option eta</option>
+                    <option value="theta">Option theta</option>
+                  </select>
+                </div>
+                <!-- Break -->
+                <div class="col-4 col-12-small">
+                  <input type="radio" id="radio-alpha" name="radio" checked>
+                  <label for="radio-alpha">Radio alpha</label>
+                </div>
+                <div class="col-4 col-12-small">
+                  <input type="radio" id="radio-beta" name="radio">
+                  <label for="radio-beta">Radio beta</label>
+                </div>
+                <div class="col-4 col-12-small">
+                  <input type="radio" id="radio-gamma" name="radio">
+                  <label for="radio-gamma">Radio gamma</label>
+                </div>
+                <!-- Break -->
+                <div class="col-6 col-12-small">
+                  <input type="checkbox" id="checkbox-alpha" name="checkbox">
+                  <label for="checkbox-alpha">Checkbox alpha</label>
+                </div>
+                <div class="col-6 col-12-small">
+                  <input type="checkbox" id="checkbox-beta" name="checkbox" checked>
+                  <label for="checkbox-beta">Checkbox beta</label>
+                </div>
+                <!-- Break -->
+                <div class="col-12">
+                  <textarea name="textarea" id="textarea" placeholder="Alpha beta gamma delta" rows="6"></textarea>
+                </div>
+                <!-- Break -->
+                <div class="col-12">
+                  <ul class="actions">
+                    <li><input type="submit" value="Submit Form" class="primary" /></li>
+                    <li><input type="reset" value="Reset" /></li>
+                  </ul>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </section>

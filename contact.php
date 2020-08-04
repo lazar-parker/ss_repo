76eea45673a75e5b12a1c1827e3c5636ab813767
DJPorkChop628 <plazar@emich.edu>
2020-08-04 12:24:45 -0400

    orange cremacicle, chillin like damn we fuckin hongry

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
   if($_POST["name"])
   {
     $name = $_POST["name"];
-    $mail = $_POST["mailing_address"];
+    $mail = $_POST["email"];
     $message = $_POST["message"];
     DB::storeContact($name, $mail, $message);
   }
@@ -25,7 +25,7 @@
         <div class="inner">
           <div class="content">
             <p>Interested in getting in contact with Summit Street A Cappella? Fill out the form below, and one of our E-Board members will be in touch shortly!</p>
-            <form method="post" action="#">
+            <form method="post" action="">
               <div class="row gtr-uniform">
                 <div class="col-6 col-12-xsmall">
                   <input type="text" name="name" id="name" value="" placeholder="Name" />
@@ -35,43 +35,7 @@
                 </div>
                 <!-- Break -->
                 <div class="col-12">
-                  <select name="category" id="category">
-                    <option value="">- Select -</option>
-                    <option value="alpha">Option alpha</option>
-                    <option value="beta">Option beta</option>
-                    <option value="gamma">Option gamma</option>
-                    <option value="delta">Option delta</option>
-                    <option value="epsilon">Option epsilon</option>
-                    <option value="zeta">Option zeta</option>
-                    <option value="eta">Option eta</option>
-                    <option value="theta">Option theta</option>
-                  </select>
-                </div>
-                <!-- Break -->
-                <div class="col-4 col-12-small">
-                  <input type="radio" id="radio-alpha" name="radio" checked>
-                  <label for="radio-alpha">Radio alpha</label>
-                </div>
-                <div class="col-4 col-12-small">
-                  <input type="radio" id="radio-beta" name="radio">
-                  <label for="radio-beta">Radio beta</label>
-                </div>
-                <div class="col-4 col-12-small">
-                  <input type="radio" id="radio-gamma" name="radio">
-                  <label for="radio-gamma">Radio gamma</label>
-                </div>
-                <!-- Break -->
-                <div class="col-6 col-12-small">
-                  <input type="checkbox" id="checkbox-alpha" name="checkbox">
-                  <label for="checkbox-alpha">Checkbox alpha</label>
-                </div>
-                <div class="col-6 col-12-small">
-                  <input type="checkbox" id="checkbox-beta" name="checkbox" checked>
-                  <label for="checkbox-beta">Checkbox beta</label>
-                </div>
-                <!-- Break -->
-                <div class="col-12">
-                  <textarea name="textarea" id="textarea" placeholder="Alpha beta gamma delta" rows="6"></textarea>
+                  <textarea name="message" id="message" placeholder="Enter your message here..." rows="6"></textarea>
                 </div>
                 <!-- Break -->
                 <div class="col-12">

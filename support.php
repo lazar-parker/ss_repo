c3fecbff00f4d82620de47daec1c2f762a136280
Parker Lazar <plazar@emich.edu>
2021-01-13 08:37:10 -0500

    switch donate to support

--- support.php
+++ support.php
@@ -6,109 +6,44 @@
 ?>
     <!-- Heading -->
       <div id="heading" >
-        <h1>Donate</h1>
+        <h1>Support Us</h1>
       </div>
 
     <!-- Main -->
 		<section class="wrapper">
 			<div class="inner">
 				<header class="special">
-					<h2>Want to help us make our mark on A Cappella?</h2>
-					<h3>Consider becoming a patron on Patreon!</h3>
+					<h2>Want to help us make our mark on collegiate A Cappella?</h2>
+					<h3>Consider supporting us through the links below!</h3>
 				</header>
-				<p>We're partnering with Patreon to help streamline the donation process, and ensure your donation is processed quickly and securely.</p>
-        <p>For more information, click <a href="https://www.patreon.com/summitstreet" target="_blank">here</a>!</p>
 				<div class="highlights">
-					<section>
-						<div class="content">
-							<header>
-								<a class="icon fa-line-chart"><span class="label">Icon</span></a>
-								<h3>Base</h3>
-								<h5>$10/year</h5>
-							</header>
-							<p>Perks:</p>
-							<ul>
-            		<li>Your company or name listed in performance programs for the year</li>
-            		<li>Two (2) complimentary Summit Street Buttons</li>
-          		</ul>
-						</div>
-					</section>
-					<section>
-						<div class="content">
-							<header>
-								<a class="icon fa-line-chart"><span class="label">Icon</span></a>
-								<h3>Slope</h3>
-								<h5>$25/year</h5>
-							</header>
-							<p>Perks:</p>
-							<ul>
-            		<li>Quarter-page ad in performance programs for the year</li>
-            		<li>Five (5) complimentary Summit Street Buttons</li>
-          		</ul>
-						</div>
-					</section>
-					<section>
-						<div class="content">
-							<header>
-								<a class="icon fa-line-chart"><span class="label">Icon</span></a>
-								<h3>Crest</h3>
-								<h5>$50/year</h5>
-							</header>
-							<p>Perks:</p>
-							<ul>
-								<li>Half-page ad in performance programs for the year</li>
-								<li>Five (5) complimentary Summit Street Buttons</li>
-								<li>One (1) complimentary Summit Street T-Shirt</li>
-							</ul>
-						</div>
-					</section>
-					<section>
-						<div class="content">
-							<header>
-								<a class="icon fa-line-chart"><span class="label">Icon</span></a>
-								<h3>Peak</h3>
-								<h5>$100/year</h5>
-							</header>
-							<p>Perks:</p>
-							<ul>
-								<li>Full-page ad in performance programs for the year</li>
-								<li>Five (5) complimentary Summit Street Buttons</li>
-								<li>One (1) complimentary Summit Street T-Shirt</li>
-								<li>Special recognition and shoutout at performances</li>
-							</ul>
-						</div>
-					</section>
-					<section>
-						<div class="content">
-							<header>
-								<a class="icon fa-line-chart"><span class="label">Icon</span></a>
-								<h3>Summit</h3>
-								<h5>$200/year</h5>
-							</header>
-							<p>Perks:</p>
-							<ul>
-            		<li>Full-page ad in performance programs for the year</li>
-            		<li>Ten (10) complimentary Summit Street Buttons</li>
-            		<li>Four (4) complimentary Summit Street T-Shirts</li>
-            		<li>Special recognition and shoutout at performances</li>
-            		<li>Early Access to new Summit Street merchandise</li>
-          		</ul>
-						</div>
-					</section>
-					<section>
-						<div class="content">
-							<header>
-								<a class="icon fa-line-chart"><span class="label">Icon</span></a>
-								<h3>Other</h3>
-								<h5>Any amount</h5>
-							</header>
-							<p>Perks:</p>
-							<ul>
-            		<li>Company or name listed in performance programs for the year</li>
-            		<li>All qualifying perks for tiers valued at or below your donation amount</li>
-          		</ul>
-						</div>
-					</section>
+          <section>
+            <div class="content">
+              <header>
+                <a href="https://www.patreon.com/summitstreet" class="icon fa-vcard-o"><span class="label">Icon</span></a>
+                <h3>Patreon</h3>
+              </header>
+              <p>Become a patron on Patreon!</p>
+            </div>
+          </section>
+          <section>
+            <div class="content">
+              <header>
+                <a href="https://www.redbubble.com/people/SummitStreet/shop" class="icon fa-line-chart"><span class="label">Icon</span></a>
+                <h3>Redbubble</h3>
+              </header>
+              <p>Buy some Summit Street merch!</p>
+            </div>
+          </section>
+          <section>
+            <div class="content">
+              <header>
+                <a href="https://www.youtube.com/channel/UCSw7AN_lhD1guTVhrmSFdAw" class="icon fa-floppy-o"><span class="label">Icon</span></a>
+                <h3>YouTube</h3>
+              </header>
+              <p>Subscribe to our YouTube!</p>
+            </div>
+          </section>
 				</div>
 			</div>
 		</section>

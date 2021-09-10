f33eb9050e4896ff3e480c99501bc5327ec35d59
Parker Lazar <plazar@emich.edu>
2021-09-10 16:37:29 -0400

    removed out-of-date elements of the website

--- support.php
+++ support.php
@@ -20,15 +20,6 @@
           <section>
             <div class="content">
               <header>
-                <a href="https://www.patreon.com/summitstreet" class="icon fa-vcard-o"><span class="label">Icon</span></a>
-                <h3>Patreon</h3>
-              </header>
-              <p>Become a patron on Patreon!</p>
-            </div>
-          </section>
-          <section>
-            <div class="content">
-              <header>
                 <a href="https://www.redbubble.com/people/SummitStreet/shop" class="icon fa-line-chart"><span class="label">Icon</span></a>
                 <h3>Redbubble</h3>
               </header>
@@ -45,17 +36,6 @@
             </div>
           </section>
 				</div>
-        <div class="inner">
-          <header class="special">
-            <h4>Interested in a virtual Singing Valentine's?</h4>
-            <p>For a minimium $5 donation, you can get access to one of three Singing Valentine's performances. Just click the "Donate" button below to get started!</p>
-            <form action="https://www.paypal.com/donate" method="post" target="_blank">
-              <input type="hidden" name="hosted_button_id" value="T6VQGHBH7KMEL" />
-              <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-              <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-            </form>
-          </header>
-        </div>
 			</div>
 		</section>
 
